923224c9a64ccb6b8a606effea3c37039673a91b
sonjamaas <sonja.maas22@web.de>
2023-11-13 10:23:29 +0100

    comments and pdf fix

--- Steigerwald_exercise.R
+++ Steigerwald_exercise.R
@@ -2,6 +2,7 @@
 install.packages("RCurl")
 library(RCurl)
 
+#explore the dataset
 getwd()
 df <- read.csv("Steigerwald_sample_points_all_data_subset_withNames.csv")  #read the csv
 head(df)
@@ -20,6 +21,7 @@ library(carData)
 #PLotting
 plot(df[df["SRTM"]>400,c("TimeScan.NDVImax","SRTM")])
 
+#recreate cover image
 library(ggplot2)
 #ggplot2
 ggplot(df, aes(x=L8.ndvi, y=L8.savi))+
@@ -27,12 +29,19 @@ ggplot(df, aes(x=L8.ndvi, y=L8.savi))+
   geom_point(aes(color=LCname),size=2)+
   facet_grid(.~LCname)
 
-pdf("landcover_vs_Lasavi_ndvi_pdf")
+#export plot as pdf
+pdf("landcover_vs_Lasavi_ndvi.pdf",width=12,height=4) 
+ggplot(df, aes(x=L8.ndvi, y=L8.savi))+
+  # geom_point(size=2)+
+  geom_point(aes(color=LCname),size=2)+
+  facet_grid(.~LCname)
+dev.off()
 
-
+#compare minimal and maximal NDVI values of different surfaces
 ggplot(df,aes(x=LCname,y=TimeScan.NDVImax))+
   geom_point(aes(color=LCname),size=2)
 
+#compare minimal and maximal NDVI
 ggplot(df,aes(factor(LCname),color='red')) +
   geom_point(aes(y=TimeScan.NDVImax),shape="triangle",color='black') +
   geom_point(aes(y=TimeScan.NDVImin),shape="square",color='yellow')
