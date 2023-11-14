b09384ea09c2aac73cd2be05b09f6ec0dfb93d97
sonjamaas <sonja.maas22@web.de>
2023-11-14 07:18:25 +0100

    added further options to plot

--- Steigerwald_exercise.R
+++ Steigerwald_exercise.R
@@ -22,6 +22,7 @@ install.packages("ggthemes")
 library(ggthemes)
 install.packages("viridis")
 library(viridis)
+library(ggplot2)
 
 #PLotting
 plot(df[df["SRTM"]>400,c("TimeScan.NDVImax","SRTM")])
@@ -36,6 +37,54 @@ ggplot(df,aes(x=L8.ndvi,y=L8.savi))+
 #add information using colour
 ggplot(df,aes(x=L8.ndvi,y=L8.savi, colour=SRTM))+
   geom_point()
+
+#adding smoothed lines
+ggplot(df,aes(x=L8.ndvi,y=L8.savi, colour=SRTM))+
+  geom_point()+
+  geom_smooth()
+
+#split the plot by landcover
+ggplot(df,aes(x=L8.ndvi,y=L8.savi, colour=SRTM))+
+  geom_point()+
+  geom_smooth()+
+  facet_wrap(~LCname)
+
+#plot SAVI values per landcover class
+ggplot()+
+  geom_point(data=df,aes(LCname, L8.savi))
+
+#add colour
+ggplot()+
+  geom_point(data=df,aes(LCname, L8.savi,colour=SRTM))
+
+#try boxplot with point "jitter"
+ggplot(df,aes(x=LCname, y=L8.savi))+
+  geom_boxplot(alpha=.5)
+
+#to see number of points, add jitter
+ggplot(df,aes(x=LCname, y=L8.savi))+
+  geom_boxplot(alpha=.5)+
+  geom_point(aes(color=SRTM),alpha=.7, size=1.5, position=position_jitter((width=.25)))
+
+#further options
+ggplot(df,aes(x=LCname, y=L8.savi))+
+  geom_jitter()
+
+#adding colour
+ggplot(df,aes(x=LCname, y=L8.savi,color=SRTM))+
+  geom_jitter()
+
+#further options
+ggplot(df,aes(x=LCname, y=L8.savi))+
+  geom_violin()
+
+ggplot(df,aes(x=TimeScan.NDVIavg, fill=LCname))+
+  geom_density(alpha=.3)
+
+#combining geoms
+ggplot(df,aes(x=LCname, y=L8.savi,color=SRTM))+
+  geom_jitter(aes(alpha=SRTM, size=TimeScan.NDVIsd,colour=L8.ndvi))+
+  geom_boxplot(alpha=.5)
 
 #recreate cover image
 library(ggplot2)
