56825f2d306b18644c2e725b407b5ae42b2d5b95
sonjamaas <sonja.maas22@web.de>
2023-11-22 18:58:29 +0100

    nice

--- germanyPlot.R
+++ germanyPlot.R
@@ -2,6 +2,7 @@ install.packages("sf")
 library(sf)
 library(ggplot2)
 library(rworldmap)
+library(ggspatial)
 #install.packages("rworldmap")
 world <- getMap()
 
@@ -11,7 +12,7 @@ plot(world)
 View(world)
 
 ger <- world[(which(world$NAME=="Germany")),]                                    #select germany from the world dataset
-
+ger_sf <- st_as_sf(ger)
 
 ggplot() +
   geom_sf(data = ger_sf, fill = "lightblue", color = "black") +
@@ -19,4 +20,10 @@ ggplot() +
   ggtitle("Germany")
 
 
+ggplot()+
+  geom_sf(data=ger_sf)+
+  annotation_scale(location="bl", width_hint=0.5)+
+  annotation_north_arrow(location="bl", which_north="true",
+                         pad_x=unit(0.2,"in"), pad_y=unit(0.5,"in"),
+                         style=north_arrow_fancy_orienteering)
 
